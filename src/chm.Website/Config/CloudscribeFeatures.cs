0b6a5f0d8df53dc50f3c55852a82e0173e40f91f
Joe Audette <joeaudette@users.noreply.github.com>
2019-02-13 11:04:01 -0500

    initial solution/project

---
+++ src/chm.Website/Config/CloudscribeFeatures.cs
@@ -0,0 +1,66 @@
+using Microsoft.Extensions.Configuration;
+
+namespace Microsoft.Extensions.DependencyInjection
+{
+    public static class CloudscribeFeatures
+    {
+
+        public static IServiceCollection SetupDataStorage(
+            this IServiceCollection services,
+            IConfiguration config,
+            bool useHangfire
+            )
+        {
+            var connectionString = config.GetConnectionString("EntityFrameworkConnection");
+
+
+
+            services.AddCloudscribeCorePostgreSqlStorage(connectionString);
+            services.AddCloudscribeLoggingPostgreSqlStorage(connectionString);
+
+            services.AddCloudscribeSimpleContentPostgreSqlStorage(connectionString);
+
+
+
+
+
+
+
+
+
+
+
+            return services;
+        }
+
+        public static IServiceCollection SetupCloudscribeFeatures(
+            this IServiceCollection services,
+            IConfiguration config
+            )
+        {
+
+            services.AddCloudscribeLogging(config);
+
+
+            services.AddCloudscribeSimpleContactForm(config);
+            services.AddScoped<cloudscribe.Web.Navigation.INavigationNodePermissionResolver, cloudscribe.Web.Navigation.NavigationNodePermissionResolver>();
+            services.AddScoped<cloudscribe.Web.Navigation.INavigationNodePermissionResolver, cloudscribe.SimpleContent.Web.Services.PagesNavigationNodePermissionResolver>();
+            services.AddCloudscribeCoreMvc(config);
+            services.AddCloudscribeCoreIntegrationForSimpleContent(config);
+            services.AddSimpleContentMvc(config);
+            services.AddContentTemplatesForSimpleContent(config);
+
+            services.AddMetaWeblogForSimpleContent(config.GetSection("MetaWeblogApiOptions"));
+            services.AddSimpleContentRssSyndiction();
+
+
+
+
+
+
+
+            return services;
+        }
+
+    }
+}
