d56b97964458f708a966476823d4248512dea404
Crispin Flower <crispinflower@esdm.co.uk>
2020-07-09 08:52:17 +0100

    #46 wip

---
+++ src/chm.Website/ViewModels/ContentWithImageAndListOfLinksModel.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace cloudscribe.SimpleContent.ContentTemplates.ViewModels
+{
+    public class ContentWithImageAndListOfLinksModel
+    {
+        public ContentWithImageAndListOfLinksModel()
+        {
+            Items = new List<CHMListItemModel>();
+        }
+
+        public string Content { get; set; }
+        [Display(Name = "Image URL (relative or absolute)")]
+        public string Image { get; set; }
+        [Display(Name = "Image caption/copyright")]
+        public string ImageCaption { get; set; }
+        public List<CHMListItemModel> Items { get; set; }
+    }
+
+    public class CHMListItemModel
+    {
+        public string LinkText { get; set; }
+        public string LinkUrl { get; set; }
+
+        public int Sort { get; set; } = 3;
+
+        public bool OpensInNewWindow { get; set; }
+    }
+}
